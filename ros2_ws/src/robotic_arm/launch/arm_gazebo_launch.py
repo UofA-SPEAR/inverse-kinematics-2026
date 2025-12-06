6c2890e062292edf948cfec62e60b00a4995fea6
hullabi <okonkwolevi2005@gmail.com>
2025-12-05 18:15:26 -0700

    Imported new instructions and merged urdf package with filename

---
+++ ros2_ws/src/robotic_arm/launch/arm_gazebo_launch.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+def generate_launch_description():
+ 
+    # Define filenames
+    urdf_package = 'robotic_arm'
+    urdf_filename = 'arm_assembly.urdf'
+    # We don't have an rviz file yet so the line below is not useful"
+    rviz_config_filename = '.......'

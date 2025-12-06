65e1db5b6bfae5dd868ab8bd2c08df448e4cae16
hullabi <okonkwolevi2005@gmail.com>
2025-12-06 14:30:09 -0700

    Refactor arm_gazebo_launch.py to include necessary imports and define package path for URDF file

--- ros2_ws/src/robotic_arm/launch/arm_gazebo_launch.py
+++ ros2_ws/src/robotic_arm/launch/arm_gazebo_launch.py
@@ -1,8 +1,7 @@
-
-
-
-
-
+from launch import launch_description
+from launch.actions import node
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 
 
 
@@ -14,3 +13,7 @@ def generate_launch_description():
     urdf_filename = 'arm_assembly.urdf'
     # We don't have an rviz file yet so the line below is not useful"
     rviz_config_filename = '.......'
+
+    pkg_share_description = FindPackageShare(urdf_package)
+    pkg_file_path_merge = PathJoinSubstitution(pkg_share_description, 'urdf', urdf_filename)
+

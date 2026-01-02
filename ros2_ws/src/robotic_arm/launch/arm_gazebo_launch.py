1ec22aaa138bfc99793771771a6f8b4de18d877f
hullabi <okonkwolevi2005@gmail.com>
2026-01-02 10:46:43 -0700

    arm_gazebo_launch Merge file paths

--- ros2_ws/src/robotic_arm/launch/arm_gazebo_launch.py
+++ ros2_ws/src/robotic_arm/launch/arm_gazebo_launch.py
@@ -1,9 +1,6 @@
 from launch import launch_description
 from launch.actions import node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
-
-
 
 
 def generate_launch_description():

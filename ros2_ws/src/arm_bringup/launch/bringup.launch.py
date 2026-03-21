819e9e4f5606345e46af92fdf51f68d0fbe79baf
nathan-correia <nathanc54@shaw.ca>
2026-03-21 17:05:56 -0600

    python launch file

--- ros2_ws/src/arm_bringup/launch/bringup.launch.py
+++ ros2_ws/src/arm_bringup/launch/bringup.launch.py
@@ -1,33 +1,109 @@
 import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch_ros.parameter_descriptions import ParameterValue
-from launch.substitutions import Command
-from launch_ros.substitutions import FindPackageShare
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from moveit_configs_utils import MoveItConfigsBuilder
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterFile
 
-pkg_share = FindPackageShare(package='arm_description').find('arm_description')
-urdf_file = os.path.join(pkg_share, 'urdf', 'robot.urdf.xacro')
-controller_config = os.path.join(pkg_share, 'config', 'ros_controllers.yaml')
 
 def generate_launch_description():
-    robot_description = ParameterValue(
-        Command(['xacro ', '/inverse-kinematics-2026/ros2_ws/src/arm_description/urdf/robot.urdf.xacro']),
-        value_type=str
+    moveit_config = (
+        MoveItConfigsBuilder("kipp", package_name="robot_moveit_config")
+        .robot_description_kinematics()
+        .to_moveit_configs()
+    )
+
+    servo_params_path = os.path.join(
+        get_package_share_directory("robot_moveit_config"),
+        "config", "servo_params.yaml"
     )
 
     robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[{'robot_description': robot_description}]
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[moveit_config.robot_description],
     )
 
-    controller_manager = Node(
-        package='controller_manager',
-        executable='ros2_control_node',
+    ros2_control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        output="screen",
         parameters=[
-            {'robot_description': robot_description},
-            '~/inverse-kinematics-2026/src/arm_bringup/config//ros2_controllers.yaml'
-        ]
+            moveit_config.robot_description,
+            os.path.join(
+                get_package_share_directory("robot_moveit_config"),
+                "config", "ros2_controllers.yaml"
+            ),
+        ],
     )
 
-    return LaunchDescription([robot_state_publisher, controller_manager])
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    arm_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["arm_controller"],
+    )
+
+    joy_node = Node(
+        package="joy",
+        executable="joy_node",
+        name="joy_node",
+    )
+
+    servo_node = Node(
+        package="moveit_servo",
+        executable="servo_node_main",
+        name="servo_node",
+        output="screen",
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+            ParameterFile(servo_params_path, allow_substs=True),
+        ],
+    )
+
+    move_group = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("robot_moveit_config"),
+                "launch", "move_group.launch.py"
+            )
+        )
+    )
+
+    rviz_config = os.path.join(
+        get_package_share_directory("arm_bringup"),
+        "config", "arm_moveit.rviz"
+    )
+
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.robot_description_kinematics,
+        ],
+    )
+
+    return LaunchDescription([
+        robot_state_publisher,
+        ros2_control_node,
+        joint_state_broadcaster_spawner,
+        arm_controller_spawner,
+        joy_node,
+        servo_node,
+        move_group,
+        rviz_node,
+    ])

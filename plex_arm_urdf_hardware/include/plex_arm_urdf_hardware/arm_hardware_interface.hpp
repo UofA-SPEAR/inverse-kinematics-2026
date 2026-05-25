6f1a63e47f8d39450e0e7622c1586fc2e2320371
hullabi <okonkwolevi2005@gmail.com>
2026-05-25 09:42:41 -0600

    hardware_interface plugin

---
+++ plex_arm_urdf_hardware/include/plex_arm_urdf_hardware/arm_hardware_interface.hpp
@@ -0,0 +1,16 @@
+#ifndef ARM_HARDWARE_INTERFACE_HPP
+#define ARM_HARDWARE_INTERFACE_HPP
+
+#include "hardware_interface/system_interface.hpp"
+
+namespace arm_hardware {
+
+    class ArmHardwareInterface : public hardware_interface::SystemInterface
+{
+public:
+    
+};
+
+}
+
+#endif

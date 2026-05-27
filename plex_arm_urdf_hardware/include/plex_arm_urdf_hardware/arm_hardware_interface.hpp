e039acb905b8b758989b67f1465a221021413262
hullabi <okonkwolevi2005@gmail.com>
2026-05-27 14:07:45 -0600

    begain implementing methods in constructor

--- plex_arm_urdf_hardware/include/plex_arm_urdf_hardware/arm_hardware_interface.hpp
+++ plex_arm_urdf_hardware/include/plex_arm_urdf_hardware/arm_hardware_interface.hpp
@@ -2,13 +2,19 @@
 #define ARM_HARDWARE_INTERFACE_HPP
 
 #include "hardware_interface/system_interface.hpp"
+#include "rclcpp_lifecycle/node_interfaces/lifecycle_node_interface.hpp"
+#include "rclcpp_lifecycle/state.hpp"
 
 namespace arm_hardware {
 
-    class ArmHardwareInterface : public hardware_interface::SystemInterface
-{
+    class ArmHardwareInterface : public hardware_interface::SystemInterface {
 public:
-    
+    ArmHardwareInterface(){
+    }
+
+    virtual rclcpp_lifecycle::node_interfaces::LifecycleNodeInterface::CallbackReturn
+    on_configure(const rclcpp_lifecycle::State & previous_state) override;
+
 };
 
 }
